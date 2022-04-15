2f164bcd2478804b3a53f8ac913763e4f340edda
deniselupe <dgrodriguez24@hotmail.com>
2022-04-15 23:33:48 +0000

    feat: collect and clean attendee phone numbers

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -6,6 +6,19 @@ attendees = CSV.open('../event_attendees.csv', headers: true, header_converters:
 template_letter = File.read('../form_letter.erb')
 erb_template = ERB.new(template_letter)
 
+def clean_phone_number(phone_num)
+  phone_num = phone_num.to_s.gsub(/[^0-9]/, '')
+  error_msg = 'Invaid number'
+
+  phone_num = error_msg if phone_num.length < 10 || phone_num.length > 11
+
+  if phone_num.length == 11
+    phone_num = phone_num[0] == '1' ? phone_num[1..11] : error_msg
+  end
+
+  phone_num
+end
+
 def clean_zipcode(zipcode)
   zipcode.to_s.rjust(5, '0')[0..4]
 end
@@ -16,13 +29,11 @@ def gather_legislators(zipcode)
   civic_info.key = 'AIzaSyClRzDqDh5MsXwnCWi0kOiiBivP6JsSyBw'
 
   begin
-    legislators = civic_info.representative_info_by_address(
-    address: zipcode,
-    levels: 'country',
-    roles: ['legislatorUpperBody', 'legislatorLowerBody']
-    )
-
-    legislators = legislators.officials
+    civic_info.representative_info_by_address(
+      address: zipcode,
+      levels: 'country',
+      roles: %w[legislatorUpperBody legislatorLowerBody]
+    ).officials
   # In case zip code is invalid or if attendee didn't provide zip code to query
   rescue
     'You can find your representatives by visiting www.commoncause.org/take-action/find-elected-officials'
@@ -34,7 +45,7 @@ def save_thank_you_letter(id, form_letter)
   Dir.mkdir('output') unless Dir.exist?('output')
   filename = "./output/thanks_#{id}.html"
 
-  File.open(filename, 'w') do  |file|
+  File.open(filename, 'w') do |file|
     file.puts form_letter
   end
 end
@@ -42,6 +53,7 @@ end
 attendees.each do |attendee|
   id = attendee[0]
   name = attendee[:first_name]
+  phone_number = clean_phone_number(attendee[:homephone])
   zipcode = clean_zipcode(attendee[:zipcode])
   legislators = gather_legislators(zipcode)
   form_letter = erb_template.result(binding)
